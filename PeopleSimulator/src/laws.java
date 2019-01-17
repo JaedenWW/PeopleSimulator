377cdbe5699252967762474453f7c068b564a1d5
Jaeden the headless <warechildJAW@gmail.com>
2019-01-17 15:14:17 +0000

    update

---
+++ PeopleSimulator/src/laws.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Law{
+    private String cName;
+    private int sentence;
+    private double chance;
+
+    public Law(String cName, int sentence, double chance){
+        this.cName = cName;
+        this.sentence = sentence;
+        this.chance = chance;
+    }
+}
